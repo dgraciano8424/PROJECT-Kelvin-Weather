a7c414e511b5f9c52d7733505846add9ff2b1287
David <176092646+dgracianoBVT2024@users.noreply.github.com>
2026-01-20 07:53:38 -0800

    first commit Kelvin Project

---
+++ script.js
@@ -0,0 +1,8 @@
+//this variable is a constant. It can't be changed from its current value of 293.
+const kelvin = 293;
+//finding out the conversion of kelvin to celsius but, subtracting 273 from the kelvin variable and storing the value of that in the celsius variable.
+const celsius = kelvin - 273;
+// used our celsius variable in an equation using math operators to find out the conversion to fahrenheit
+//We are using the Math.floor built-in function to round the value of the equation down.
+const fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+console.log("The temperature is `{$fahrenheit}`");
