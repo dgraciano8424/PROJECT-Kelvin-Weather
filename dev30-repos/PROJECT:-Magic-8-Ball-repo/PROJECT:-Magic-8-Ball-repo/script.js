56a35179766c6ea4c397bc0c080d4803c9a1b2e0
dgraciano <dgraciano5058@currently.com>
2026-01-23 08:22:16 -0800

    Refactor comments in script.js for better readability
    Updated comments for clarity and formatting.

--- dev30-repos/PROJECT:-Magic-8-Ball-repo/PROJECT:-Magic-8-Ball-repo/script.js
+++ dev30-repos/PROJECT:-Magic-8-Ball-repo/PROJECT:-Magic-8-Ball-repo/script.js
@@ -1,16 +1,20 @@
 const userName = "David";
-//console log of a ternary expression with a string interpolation embedded inside of it. If condition is string it will print "Hello (userName)!", otherwise "Hello!"
+//console log of a ternary expression with a string interpolation embedded inside of it. 
+If condition is string it will print "Hello (userName)!", otherwise "Hello!"
 console.log(String ? `Hello ${userName}!` : "Hello!");
 
 //constant variable that prints out the question from User
 const userQuestion = "Will I ever win the lottery?";
 console.log(`${userName} asked, "${userQuestion}`);
 
-//constant variable that has the value of 2 built-in methods: Math.floor and Math.random. Multiplying random by any whole number gives (n - 1). I.E. If we used 1000 i'd get 999 different choices or numbers.
+//constant variable that has the value of 2 built-in methods: Math.floor and Math.random. 
+//Multiplying random by any whole number gives (n - 1). 
+//I.E. If we used 1000 i'd get 999 different choices or numbers.
 const randomNumber = Math.floor(Math.random() * 8);
 const eightBall = randomNumber;
 
-//a switch statement taking the randomized number from variable "eightball" and using it to run the predetermined console.log depending on the RNG.
+//a switch statement taking the randomized number from 
+//variable "eightball" and using it to run the predetermined console.log depending on the RNG.
 switch (eightBall) {
   case 0:
     console.log("It is certain");
