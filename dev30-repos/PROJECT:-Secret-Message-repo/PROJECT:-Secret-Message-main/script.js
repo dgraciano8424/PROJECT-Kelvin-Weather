b9d65408bdbe34403d89dc5bfc71ed34832f6b26
David <176092646+dgracianoBVT2024@users.noreply.github.com>
2026-01-28 20:21:14 -0800

    Secret Message almost done

---
+++ dev30-repos/PROJECT:-Secret-Message-repo/PROJECT:-Secret-Message-main/script.js
@@ -0,0 +1,37 @@
+console.log("hello world");
+let secretMessage = [
+  "Learning",
+  "is",
+  "not",
+  "about",
+  "what",
+  "you",
+  "get",
+  "easily",
+  "the",
+  "first",
+  "time,",
+  "it",
+  "is",
+  "about",
+  "what",
+  "you",
+  "can",
+  "figure",
+  "out.",
+  "-2015,",
+  "Chris",
+  "Pine,",
+  "Learn",
+  "JavaScript",
+];
+
+secretMessage.pop();
+secretMessage.length;
+secretMessage.push("to", "Program");
+secretMessage.splice(7, 1, "right");
+secretMessage.shift();
+// secretMessage.push.[0]("Programming");
+console.log(secretMessage);
+secretMessage.splice(0, 0, "Programming");
+secretMessage[(0, 1, 2)] = "right";
