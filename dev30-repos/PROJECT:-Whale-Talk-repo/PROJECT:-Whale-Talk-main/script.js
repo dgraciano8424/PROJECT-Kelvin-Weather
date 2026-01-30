664f5016b0ac2a6f0a5ae3efa4a2cf1b08fddffe
David <176092646+dgracianoBVT2024@users.noreply.github.com>
2026-01-30 08:01:42 -0800

    Whale Talk start

---
+++ dev30-repos/PROJECT:-Whale-Talk-repo/PROJECT:-Whale-Talk-main/script.js
@@ -0,0 +1,14 @@
+//Step 1
+var input = "P. Sherman, 42 Wallaby Way, Sydney";
+
+//Step 2
+const vowels = ["a", "e", "i", "o", "u"];
+
+//Step 3
+var resultArray = [];
+
+//Step 4
+for (let input = 0; input < array.length; input++) {
+  const resultArray = array[input];
+  console.log(resultArray);
+}
